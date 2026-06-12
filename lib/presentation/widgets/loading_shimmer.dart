89e351161159644c0de20c3f7c19f6256303405d
shubham4329design <shubhamchopade3241@gmail.com>
2026-06-12 15:51:54 +0530

    feat: Add loading shimmer effect widget

---
+++ lib/presentation/widgets/loading_shimmer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoadingShimmer extends StatefulWidget {
+  final Widget child;
+
+  const LoadingShimmer({
+    required this.child,
+    super.key,
+  });
+
+  @override
+  State<LoadingShimmer> createState() => _LoadingShimmerState();
+}
+
+class _LoadingShimmerState extends State<LoadingShimmer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return ShaderMask(
+          shaderCallback: (bounds) {
+            return LinearGradient(
+              begin: Alignment(-1.0 - _controller.value * 2, 0.0),
+              end: Alignment(1.0 - _controller.value * 2, 0.0),
+              colors: [
+                Colors.grey[300]!,
+                Colors.grey[200]!,
+                Colors.grey[300]!,
+              ],
+              stops: const [0.0, 0.5, 1.0],
+            ).createShader(bounds);
+          },
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
